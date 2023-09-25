733f382b673139aff849ea0bfb3c6c918ad80a81
PhoonThetPine <thetpine254@gmail.com>
2023-09-25 11:19:16 +0800

    Meditation App ZenFlow - V1

---
+++ app/src/main/java/com/example/zenflow/BottomMenuContent.kt
@@ -0,0 +1,8 @@
+package com.example.zenflow
+
+import androidx.annotation.DrawableRes
+
+data class BottomMenuContent(
+    val title: String,
+    @DrawableRes val iconId: Int
+)
---
+++ app/src/main/java/com/example/zenflow/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.zenflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.zenflow.pages.HomeScreen
+import com.example.zenflow.ui.theme.ZenFlowTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalFoundationApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ZenFlowTheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
